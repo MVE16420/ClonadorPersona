ba6d5d4d6cee5a25c17820f95d02129e5262f2ef
Mañana <Maana@Aula8-08>
2024-04-26 09:46:57 +0200

    Start del clon

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,9 +1,14 @@
 package main;
 
+import clonador.ClonadorPersona;
+import persona.Persona;
+
 public class Start {
 
 	public static void main(String[] args) {
 	
+			ClonadorPersona cp = new ClonadorPersona("Lucas");
+			
 
 	}
 
--- src/utilidades/FactoryPersona.java
+++ src/utilidades/FactoryPersona.java
@@ -5,16 +5,27 @@ import persona.PersonaData;
 
 public class FactoryPersona {
 
-	public static Persona getPersona (String  nombre) {
+	public static Persona getPersona (String  nombre) throws Exception {
 		
 		Persona p = new Persona();
 	
 		    p.setNombre(nombre);
 			int edad =	(int)(Math.random()*100);
-						p.setEdad(edad);
+			if(edad<PersonaData.EDAD_MIN || edad>=PersonaData.EDAD_MAX) {
+				throw new Exception("Edad no valido");
+			}
+			p.setEdad(edad);
+			
 			int peso =(int)(Math.random()*101);
+			if(edad<PersonaData.PESO_MIN || edad>=PersonaData.PESO_MAX) {
+				throw new Exception("Peso no valido");
+			}
 			p.setPeso(peso);
+			
 			int altura = (int)(Math.random()*201);
+			if(edad<PersonaData.ALTURA_MIN || edad>=PersonaData.ALTURA_MAX) {
+				throw new Exception("Altura no valido");
+			}
 			p.setAltura(altura);
 			return p;
 	}
